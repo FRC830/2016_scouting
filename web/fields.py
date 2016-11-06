95a862067b541cd3c033b932c4f5f486583d4169
robert burke <rwburke@roberts-MBP.first>
2016-11-06 15:57:54 -0500

    scouting form form blomfield

--- web/fields.py
+++ web/fields.py
@@ -12,31 +12,52 @@ class Form(flask_wtf.Form):
     #Auton Section
     auton_start = RadioField('Robot Starting Location',choices=[('Neutral Zone','Neutral Zone'), ('Courtyard','Courtyard')],
                              default='Neutral Zone')
-    auton_breach = RadioField('Defense Crossed in Auton', choices=[('0','None'),
-                                                                   ('1','Touched'),
-                                                                   ('2','Crossed (Mark the defense in the Teleop section)')],
-                                                                   default='0')
     auton_score = RadioField('Ball Scored', choices=[('0','None'), ('1','Low Goal'), ('2','High Goal')],
                              default='0')
     #Teleop Section
 
-    #Breaching checkboxes
+    #Breaching checkboxes Teleop
     lb_breach = BreachField('Low Bar')
-    pc_breach = BreachField('Portcullis')
     cf_breach = BreachField('Cheval de Frise')
     mo_breach = BreachField('Moat')
     rp_breach = BreachField('Ramparts')
     db_breach = BreachField('Drawbridge')
-    sp_breach = BreachField('Sally Port')
     rw_breach = BreachField('Rock Wall')
     rt_breach = BreachField('Rough Terrain')
 
+    #Breaching checkboxes Auton
+    alb_breach = BreachField('Low Bar')
+    acf_breach = BreachField('Cheval de Frise')
+    amo_breach = BreachField('Moat')
+    arp_breach = BreachField('Ramparts')
+    adb_breach = BreachField('Drawbridge')
+    arw_breach = BreachField('Rock Wall')
+    art_breach = BreachField('Rough Terrain')
+
     #Other
+    autonbreach_count = IntegerField('Number of defenses crossed', default=0,
+        col_md=6,
+        label_col_md=6,
+        col_sm=8,
+        label_col_sm=12)
+
     breach_count = IntegerField('Number of defenses crossed', default=0,
         col_md=6,
         label_col_md=6,
         col_sm=8,
         label_col_sm=12)
+    autonworking_percentage = IntegerField('How long did the robot work for in percentage 0-100', default=100,
+    	col_md=6,
+        label_col_md=6,
+        col_sm=8,
+        label_col_sm=12) 
+    teleworking_percentage = IntegerField('How long did the robot work for in percentage 0-100', default=100,
+    	col_md=6,
+        label_col_md=6,
+        col_sm=8,
+        label_col_sm=12) 
+    goalsandbreach = IntegerField('Points scored from Goals and Breaches', default=0, col_sm=6)
+    certainty = IntegerField('How certain are you of the previous answer, 0-100', default=0, col_sm=6)
     high_scores = IntegerField('High Goals Scored', default=0, col_sm=6)
     high_misses = IntegerField('High Shots Missed', default=0, col_sm=6)
     low_scores = IntegerField('Low Goals Scored', default=0, col_sm=6)
